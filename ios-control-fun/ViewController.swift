b01176da45f05a2836b7d05b0b4b27e102e0c3e3
rebornwhu <rebornwhu@gmail.com>
2015-09-20 14:49:18 -0700

    Finish modify textField

--- ios-control-fun/ViewController.swift
+++ ios-control-fun/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var numberField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
